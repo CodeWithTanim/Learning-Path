cbc2499503c2e67a4b032af873b8957b38d86f9a
MD SAMIUR RAHMAN TANIM <codewithtanim+support@gmail.com>
2025-11-01 14:52:44 +0530

    11. String in Python

---
+++ Python-Programming/11. Strings in Python/Strings.py
@@ -0,0 +1,38 @@
+name = "Tanim"
+friend_name = "Atik"
+another_friend = "Robi"
+
+print("My name is " + name + ".")
+print("My friend's name is " + friend_name + ".")
+print("Another friend's name is " + another_friend + ".") 
+
+favourite_thing = '''I
+love to write code and 
+eat biryani.'''
+
+print(name,"\'s favourite thing is: favourite_thing")
+
+x = """Hello, this is a
+multiline string.
+It can span multiple lines.
+Enjoy coding!"""
+
+print(x)
+
+# Accessing characters in a string
+name1 = "Samiur"
+print(name1)
+
+print(name1[0])  # prints S
+print(name1[1])  # prints a
+print(name1[2])  # prints m
+print(name1[3])  # prints i
+print(name1[4])  # prints u
+print(name1[5])  # prints r
+
+print("\n")
+# Looping through a string
+for character in name:
+    print(character)
+
+# This will print each character of the string one by one.
