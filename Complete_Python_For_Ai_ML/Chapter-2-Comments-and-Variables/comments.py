15d530906f5cd4489766318513e1b83222159b8c
MD. SAMIUR RAHMAN TANIM <codewithtanim+support@gmail.com>
2026-08-12 21:21:05 +0600

    Chapter 02: Comments & Variables

---
+++ Complete_Python_For_Ai_ML/Chapter-2-Comments-and-Variables/comments.py
@@ -0,0 +1,16 @@
+# this line below is used for printing
+
+print("Hello Developers")
+
+'''This is 
+multi
+line 
+comments. No issue with
+multi-line
+comments'''
+
+"""Same as like single quotes and double codes
+will works for 
+multi line
+comments"""
+
---
+++ Complete_Python_For_Ai_ML/Chapter-2-Comments-and-Variables/variable.py
@@ -0,0 +1,21 @@
+# use for store something in variables
+
+sam = 12
+print(sam)
+
+name = "Tanim"
+
+# Anything can be variables but 3 rules
+# 1. vaiable can not start with numbers
+# 2. no spaces are allowed
+# 3. no special characters, but some
+
+# Camel Case
+sheryinasXNyc = 14
+
+# Pascal Case
+SheryiansXNyc = 34
+
+# Snake Case
+sheryinas_x_nyc = 54
+
