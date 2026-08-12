fc90cc9a0b0f56af99a1a1d11114abb7321c135a
MD. SAMIUR RAHMAN TANIM <codewithtanim+support@gmail.com>
2026-08-12 20:14:24 +0600

    Start from 0 (ZERO)

--- Complete_Python_For_Ai_ML/Chapter_2_Comments_and_Variables/Comments.py
+++
@@ -1,7 +0,0 @@
-# This is single Line Comment
-
-"""Hello People,
-This is CodeWithTanim
-a Student and a Learner!         ---> This is multiline Comment
-from Bangladesh!
-studied in adtU - India"""
